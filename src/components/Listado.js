b410a115dc53261b7ec248c3ff2445e5b917ad5c
Javier Misat <rjmisal@misena.edu.co>
2018-12-14 13:44:47 -0500

    Mostrando gastos en segunda columna

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import Gasto from "./Gasto";
 
 class Listado extends Component {
     render() {
         return (
             <div className="gastos-realizados">
                 <h2>Listado</h2>
-                {console.log(this.props.gastos)}
+
+                {Object.keys(this.props.gastos).map(key =>(
+                    <Gasto
+                        key={key}
+                        gasto={this.props.gastos[key]}/>
+                    ))}
             </div>
         );
     }
