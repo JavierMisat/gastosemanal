0109b6f54d2d20c49a82f23d728ae4ae5584b018
Javier Misat <rjmisal@misena.edu.co>
2018-12-14 00:00:33 -0500

    Mi primer commit

---
+++ src/components/Formulario.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class FormularioGasto extends Component {
+
+     // refs para los campos del formulario
+     nombreGasto = React.createRef();
+     cantidadGasto = React.createRef()
+
+     crearGasto = (e) => {
+          // Prevenir el default
+          e.preventDefault();
+
+          // crear el objeto con los datos
+          const gasto = {
+               nombreGasto : this.nombreGasto.current.value,
+               cantidadGasto : this.cantidadGasto.current.value
+          }
+
+          // console.log(gasto);
+
+          // agregarlo y enviarlo por props
+          this.props.agregarGasto(gasto);
+
+          // resetear el formulario (opcional)
+          e.currentTarget.reset();
+          
+     }
+
+     render() {
+          return(
+               <form onSubmit={this.crearGasto}>
+                    <h2>Agrega tus gastos aqui</h2>
+
+                    <div className="campo">
+                         <label>Nombre Gasto</label>
+                         <input ref={this.nombreGasto} className="u-full-width" type="text" placeholder="Ej. Transporte" />
+                    </div>
+               
+                    <div className="campo">
+                         <label>Cantidad</label>
+                         <input ref={this.cantidadGasto} className="u-full-width" type="text" placeholder="Ej. 300" />
+                    </div>
+               
+                    <input className="button-primary u-full-width" type="submit" value="Agregar" />
+               </form>
+          )
+     }
+}
+
+FormularioGasto.propTypes = {
+     agregarGasto: PropTypes.func.isRequired
+}
+
+export default FormularioGasto;
