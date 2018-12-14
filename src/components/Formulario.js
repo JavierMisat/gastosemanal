307f5b36cee25a879a19b9b4c3b47e1518f5d7ce
Javier Misat <rjmisal@misena.edu.co>
2018-12-14 00:23:10 -0500

    Leyendo los datos del formulario con React.createRefs()

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,22 +1,42 @@
 import React, {Component} from 'react';
 
-class FormularioGasto extends Component{
+class FormularioGasto extends Component {
+
+    //Refs para los campos del formulario
+    nombreGasto = React.createRef();
+    cantidadGasto = React.createRef();
+
+    crearGasto = (e) => {
+        //Prevenir el default
+        e.preventDefault();
+        //Crear el objeto con los datos
+        const gasto = {
+          nombreGasto: this.nombreGasto.current.value,
+          cantidadGasto: this.cantidadGasto.current.value
+        };
+        console.log(gasto);
+
+        //Agregarlo y enviarlo a los props
+
+        //Resetear el formulario (opcional)
+        e.currentTarget.reset();
+    };
 
     render() {
         return (
-            <form>
+            <form onSubmit={this.crearGasto}>
                 <h2>Agrega tus gastos aqui</h2>
                 <div className="campo">
                     <label>Nombre Gasto</label>
-                    <input className="u-full-width" type="text" placeholder="Ej. Transporte" />
+                    <input ref={this.nombreGasto} className="u-full-width" type="text" placeholder="Ej. Transporte"/>
                 </div>
 
                 <div className="campo">
                     <label>Cantidad</label>
-                    <input className="u-full-width" type="text" placeholder="Ej. 300" />
+                    <input ref={this.cantidadGasto} className="u-full-width" type="text" placeholder="Ej. 300"/>
                 </div>
 
-                <input className="button-primary u-full-width" type="submit" value="Agregar" />
+                <input className="button-primary u-full-width" type="submit" value="Agregar"/>
             </form>
         );
     }
