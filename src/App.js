0b814246b4026c1655b926d902b35e99fe6276b5
Javier Misat <rjmisal@misena.edu.co>
2018-12-14 00:14:02 -0500

    Creado formulario de gastos falta leer datos

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './css/App.css';
+import Header from "./components/Header";
+import FormularioGasto from "./components/Formulario";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app container">
+                <Header
+                    titulo="Gasto Semanal"/>
+
+                <div className="contenido-principal">
+                    <div className="row">
+                        <div className="one-half column">
+                            <FormularioGasto/>
+                        </div>
+                        <div className="one-half column">
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
